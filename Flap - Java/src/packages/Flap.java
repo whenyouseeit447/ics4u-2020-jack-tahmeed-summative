a351341623e7a54a918718503d82200b7265988f
star-fruit <52576397+star-fruit@users.noreply.github.com>
2020-05-30 18:23:31 -0400

    Updated Full Game
    Completed tutorial. Now time to work on the actual game

--- Flap - Java/src/packages/Flap.java
+++ Flap - Java/src/packages/Flap.java
@@ -5,13 +5,14 @@ import java.util.*;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-public class Flap implements ActionListener{
+public class Flap implements ActionListener, MouseListener, KeyListener{
   public static Flap flap;
   public final int WIDTH=1080, HEIGHT=720;//Should all these be public static?
   public Renderer renderer;
   public Rectangle bird;
   public Random rand;
-  public int ticks, ymotion;
+  public boolean gameOver, started = false;
+  public int ticks, ymotion, score;
   public ArrayList<Rectangle> columns;
   
   public Flap(){
@@ -23,6 +24,8 @@ public class Flap implements ActionListener{
     jframe.add(renderer);
     jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
     jframe.setSize(WIDTH,HEIGHT);
+    jframe.addMouseListener(this);
+    jframe.addKeyListener(this);
     jframe.setResizable(false);
     jframe.setTitle("Flap");
     jframe.setVisible(true);
@@ -59,10 +62,86 @@ public class Flap implements ActionListener{
     g.fillRect(column.x,column.y,column.width,column.height);
   }
   
+ public void jump(){
+   if (gameOver){
+    bird = new Rectangle(WIDTH/2-10, HEIGHT/2-10, 20,20);
+    columns.clear();
+    ymotion = 0;
+    score = 0;
+    
+    addColumn(true);
+    addColumn(true);
+    addColumn(true);
+    addColumn(true);
+    
+    gameOver= false;
+   }
+   if (!started){
+     started = true;
+   }
+   
+   else if (!gameOver) {
+     if (ymotion >0){
+       ymotion = 0;
+     }
+   }
+   ymotion -= 10;
+ }
+ 
+ @Override
   public void actionPerformed(ActionEvent e){
-    if (ticks % 2 == 0){
-      ymotion += 2;
+    int speed = 10;
+    ticks ++;
+    
+    if (started){
+      for (int i=0; i<columns.size(); i++){
+        Rectangle column = columns.get(i);
+        column.x -= 10;
+        if (column.x + column.width <0){
+          columns.remove(column);
+          if (column.y==0){
+           addColumn(false);
+          }
+        }
+      }
+
+      if ((ticks % 2 == 0) && (ymotion<15)){
+        ymotion += 2;
+      }
+      bird.y += ymotion;
+      for (Rectangle column: columns){
+        
+        if (column.y == 0 && bird.x + bird.width / 2 > column.x + column.width /2 -10 && bird.x+ bird.width /2 < column.x + column.width / 2 +10){
+          score ++;
+        }
+        
+        if (column.intersects(bird)){
+          gameOver = true;
+          if (bird.x<=column.x){
+            bird.x = column.x-bird.width;
+          }
+          else{
+            if (column.y != 0){
+              bird.y = column.y-bird.height;
+            }
+            else if  (bird.y<column.height){
+              bird.y = column.height;
+            }
+          }
+          bird.x = column.x - bird.width;
+        }
+      }
     }
+  
+  if ((bird.y > HEIGHT - 150) ||(bird.y<0)) {
+    bird.y = HEIGHT - 150;
+    gameOver = true;
+  }
+  
+  if (bird.y + ymotion >= HEIGHT - 150){
+    bird.y = HEIGHT -120 - bird.height;
+  }
+  
     renderer.repaint();
   }
   
@@ -78,9 +157,68 @@ public class Flap implements ActionListener{
     
     g.setColor(Color.RED);
     g.fillRect(bird.x, bird.y, bird.width, bird.height);
-
+    
+    //iterator
+    for (Rectangle column : columns){
+      paintColumn(g, column);
+    }
+    
+    g.setColor(Color.white);
+    g.setFont(new Font("Arial",1,100));
+    
+    if (!started){
+      g.drawString("Click to start!", 250, HEIGHT / 2 -100);
+    }
+    
+    if (gameOver){
+      g.drawString("Game Over!",250,HEIGHT/2-100);
+    }
+    
+    if (!gameOver && started){
+      g.drawString (String.valueOf(score), WIDTH / 2 -25, 100);
+    }
   }
   
+  @Override
+  public void keyReleased(KeyEvent e){
+    if (e.getKeyCode() == KeyEvent.VK_SPACE){
+	jump();
+      }
+  }
+  
+  
+  //These override statements are necessary even if they dont do anything atm. We'll see if we can delete them later
+  //If it works, it works. 
+  
+  @Override
+  public void mouseClicked(MouseEvent e){
+    jump();
+  }
+  
+  @Override
+  public void mousePressed(MouseEvent e){
+  }
+  
+  @Override
+  public void mouseReleased(MouseEvent e){
+  }
+  
+  @Override
+  public void mouseEntered(MouseEvent e){
+  }
+  
+  @Override
+  public void mouseExited(MouseEvent e){
+  }
+  
+  @Override
+  public void keyTyped(KeyEvent e){
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e){
+
+  }
   
   public static void main(String[] args) {
    flap = new Flap(); 
